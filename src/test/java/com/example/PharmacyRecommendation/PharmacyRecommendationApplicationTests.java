6d30bbc9f36753ae85ea11e009e9e986e4963e0e
Jeongyounghyeon <snorlax1106@naver.com>
2024-01-03 16:51:55 +0900

    chore: init project

---
+++ src/test/java/com/example/PharmacyRecommendation/PharmacyRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PharmacyRecommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyRecommendationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
