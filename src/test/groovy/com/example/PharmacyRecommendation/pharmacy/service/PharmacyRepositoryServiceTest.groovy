f928750f7217059dc15d3816c9d51ff39482f855
Jeongyounghyeon <snorlax1106@naver.com>
2024-01-05 15:44:25 +0900

    test: PharmacyRepositoryService dirty checking test 추가

---
+++ src/test/groovy/com/example/PharmacyRecommendation/pharmacy/service/PharmacyRepositoryServiceTest.groovy
@@ -0,0 +1,70 @@
+package com.example.PharmacyRecommendation.pharmacy.service
+
+import com.example.PharmacyRecommendation.AbstractIntegrationContainerBaseTest
+import com.example.PharmacyRecommendation.pharmacy.entity.Pharmacy
+import com.example.PharmacyRecommendation.pharmacy.repository.PharmacyRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.testcontainers.shaded.org.bouncycastle.asn1.crmf.SinglePubInfo
+
+class PharmacyRepositoryServiceTest extends AbstractIntegrationContainerBaseTest {
+
+    @Autowired
+    private PharmacyRepositoryService pharmacyRepositoryService
+
+    @Autowired
+    private PharmacyRepository pharmacyRepository
+
+    def setup() {
+        pharmacyRepository.deleteAll()
+    }
+
+    def "PharmacyRepository update - dirty checking success"() {
+        given:
+        String inputAddress = "서울 특별시 성북구 종암동"
+        String modifiedAddress = "서울 광진구 구의동"
+        String name = "은혜 약국"
+        double latitude = 36.11
+        double longitude = 128.11
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(inputAddress)
+                .pharmacyName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build()
+
+        when:
+        def entity = pharmacyRepository.save(pharmacy)
+        pharmacyRepositoryService.updateAddress(entity.getId(), modifiedAddress)
+
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.get(0).getPharmacyAddress() == modifiedAddress
+    }
+
+    def "PharmacyRepository update - dirty checking fail"() {
+        given:
+        String inputAddress = "서울 특별시 성북구 종암동"
+        String modifiedAddress = "서울 광진구 구의동"
+        String name = "은혜 약국"
+        double latitude = 36.11
+        double longitude = 128.11
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(inputAddress)
+                .pharmacyName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build()
+
+        when:
+        def entity = pharmacyRepository.save(pharmacy)
+        pharmacyRepositoryService.updateAddressWithoutTransaction(entity.getId(), modifiedAddress)
+
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.get(0).getPharmacyAddress() == inputAddress
+    }
+}
