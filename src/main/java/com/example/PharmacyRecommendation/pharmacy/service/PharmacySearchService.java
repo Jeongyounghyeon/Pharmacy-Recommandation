ee7fbfff31197fbecec6f0aa2b21682c21c2e98d
Jeongyounghyeon <snorlax1106@naver.com>
2024-01-06 15:29:41 +0900

    feat: 거리 계산 알고리즘 구현

---
+++ src/main/java/com/example/PharmacyRecommendation/pharmacy/service/PharmacySearchService.java
@@ -0,0 +1,39 @@
+package com.example.PharmacyRecommendation.pharmacy.service;
+
+import com.example.PharmacyRecommendation.pharmacy.dto.PharmacyDto;
+import com.example.PharmacyRecommendation.pharmacy.entity.Pharmacy;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PharmacySearchService {
+
+    private final PharmacyRepositoryService pharmacyRepositoryService;
+
+    public List<PharmacyDto> searchPharmacyDtoList() {
+
+        // redis
+
+        // db
+
+        return pharmacyRepositoryService.findAll()
+                .stream()
+                .map(this::convertToPharmacyDto)
+                .collect(Collectors.toList());
+    }
+
+    private PharmacyDto convertToPharmacyDto(Pharmacy pharmacy) {
+        return PharmacyDto.builder()
+                .id(pharmacy.getId())
+                .pharmacyAddress(pharmacy.getPharmacyAddress())
+                .pharmacyName(pharmacy.getPharmacyName())
+                .latitude(pharmacy.getLatitude())
+                .longitude(pharmacy.getLongitude())
+                .build();
+    }
+}
